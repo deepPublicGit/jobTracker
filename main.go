4bfe20dd8d971b3bab006347acdeba3f13cc7a27
ByronBlaze <pg903@snu.edu.in>
2025-04-22 00:40:40 +0530

    -- Basic Greenhouse Scraper.

--- main.go
+++ main.go
@@ -1,21 +1,98 @@
 package main
 
 import (
-  "fmt"
+	"encoding/csv"
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
+	"log"
+	"os"
+	"strings"
+	"time"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Job struct {
+	Title    string
+	Location string
+	Date     time.Time
+	URL      string
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	fName := "ghtest.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write([]string{"Title", "Location", "Date", "URL"})
+
+	c := colly.NewCollector(
+		colly.Debugger(&debug.LogDebugger{}),
+		// Visit only domains: coursera.org, www.coursera.org
+		colly.AllowedDomains("job-boards.greenhouse.io"),
+
+		// Cache responses to prevent multiple download of pages
+		// even if the collector is restarted
+		colly.CacheDir("./gh_cache"),
+	)
+
+	jobs := make([]Job, 0, 200)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if !strings.Contains(link, "/jobs/") {
+			return
+		}
+		//
+		//if e.DOM.Find("td[class=cell]").Length() == 0 {
+		//	return
+		//}
+		//title := strings.Split(e.ChildText(".job-title"), "\n")[0]
+		//course_id := e.ChildAttr("p[class=body body--medium]", "value")
+		////start_date, _ := time.Parse(DATE_FORMAT, e.ChildText("span.start-date"))
+		////end_date, _ := time.Parse(DATE_FORMAT, e.ChildText("span.final-date"))
+		//var run string
+		//if len(strings.Split(course_id, "_")) > 1 {
+		//	run = strings.Split(course_id, "_")[1]
+		//}
+		/*		e.ForEach("p", func(i int, ch *colly.HTMLElement) {
+				log.Printf("Raw: %s, %s, %s", ch.Name, ch.DOM, ch.Index)
+				log.Printf(ch.Text)
+			})*/
+		//log.Printf("Checking %s\n", )
+		//log.Printf("Title %s\n", e.ChildText("p.body body--medium"))
+		//log.Printf("Location %s\n", e.ChildText("p.body body__secondary body--metadata"))
+
+		job := Job{
+			Title:    e.DOM.Find("p").Eq(0).Text(),
+			Location: e.DOM.Find("p").Eq(1).Text(),
+			Date:     time.Now(),
+			URL:      link,
+		}
+		countries := []string{"India", "Hyderabad", "Chennai", "Gurgaon", "Gurugram", "Delhi", "APAC", "Bengaluru", "Bangalore", "Remote"}
+
+		for _, country := range countries {
+			if strings.Contains(strings.ToLower(job.Location), strings.ToLower(country)) {
+				jobs = append(jobs, job)
+				writer.Write([]string{job.Title, job.Location, job.Date.Format("2006-01-02"), job.URL})
+				break
+			}
+		}
+
+		// start scaping the page under the link found
+		//e.Request.Visit(link)
+	})
+	log.Printf("Collector starting...")
+
+	c.Visit("https://job-boards.greenhouse.io/workatbackbase/")
+
+	log.Printf("Scraping finished, check file %q for results\n", fName)
+	log.Printf("Done. Jobs: %d\n, %s", len(jobs), jobs)
 }
